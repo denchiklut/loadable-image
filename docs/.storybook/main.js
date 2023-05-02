69b5c29735cd95043f393e570dfb0c8d1e992d2e
Karl Aleksandrov <ollylut@gmail.com>
2023-05-02 17:06:23 +0200

    feat: storybook (#3)
    - update storybook

--- docs/.storybook/main.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-	addons: ['@storybook/addon-essentials'],
-	framework: '@storybook/react',
-	core: { builder: 'webpack5' }
-}
