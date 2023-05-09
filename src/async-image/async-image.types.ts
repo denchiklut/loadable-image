aa3304248c14c4174afd414672873d62dbb37046
denchiklut <ollylut@gmail.com>
2023-05-09 21:40:40 +0200

    feat: 2.1.4 - update types

--- src/async-image/async-image.types.ts
+++ src/async-image/async-image.types.ts
@@ -1,10 +1,14 @@
 import type { CSSProperties, ReactElement, ImgHTMLAttributes } from 'react'
 
+type PX = `${number}px`
+type RootMargin = `${PX} ${PX} ${PX} ${PX}` | `${PX} ${PX} ${PX}` | `${PX} ${PX}` | `${PX}`
+
 export type Style = CSSProperties & Required<Pick<CSSProperties, 'height' | 'width'>>
+
 export interface AsyncImageProps
 	extends Omit<ImgHTMLAttributes<HTMLImageElement>, 'height' | 'width'> {
 	src: string
-	rootMargin?: string
+	rootMargin?: RootMargin
 	error?: ReactElement
 	loader?: ReactElement
 	style: Style
