3949ccd5656e9f5752fd5449360ce6d9b1e39068
Karl Aleksandrov <ollylut@gmail.com>
2024-04-03 10:15:52 +0200

    feat: dependencies (#12)
    - updated npm dependencies

--- src/async-image/async-image.types.ts
+++ src/async-image/async-image.types.ts
@@ -9,9 +9,9 @@ import type { TransitionProps } from 'transitions-kit'
 
 type PX = `${number}px`
 type RootMargin = `${PX} ${PX} ${PX} ${PX}` | `${PX} ${PX} ${PX}` | `${PX} ${PX}` | `${PX}`
+type ImageProps = Omit<ImgHTMLAttributes<HTMLImageElement>, 'height' | 'width'>
 
 export type SourceProps = Omit<SourceHTMLAttributes<HTMLSourceElement>, 'src'> & { srcSet: string }
-export type ImageProps = Omit<ImgHTMLAttributes<HTMLImageElement>, 'height' | 'width'>
 
 export interface AsyncImageProps extends ImageProps {
 	src: string
