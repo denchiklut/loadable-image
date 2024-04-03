50a3aad40f733806a2bc97d6c2f6d711cd357291
Karl Aleksandrov <ollylut@gmail.com>
2024-04-03 10:30:42 +0200

    feat: docs (#13)
    - updated npm dependencies

--- docs/.storybook/main.ts
+++ docs/.storybook/main.ts
@@ -1,11 +1,12 @@
 import type { StorybookConfig } from '@storybook/react-webpack5'
 
 export default {
-	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(ts|tsx)'],
 	addons: [
 		'@storybook/addon-links',
 		'@storybook/addon-essentials',
-		'@storybook/addon-interactions'
+		'@storybook/addon-interactions',
+		'@storybook/addon-webpack5-compiler-babel'
 	],
 	framework: {
 		name: '@storybook/react-webpack5',
