7f61f11eeb74d0984b81bd4b0a6c6e9e71640fdf
Ray Booysen <raybooysen@gmail.com>
2024-11-06 20:11:45 +0800

    Remove the omission of width and height
    Allow height and width in props

--- src/async-image/async-image.types.ts
+++ src/async-image/async-image.types.ts
@@ -9,11 +9,10 @@ import type { TransitionProps } from 'transitions-kit'
 
 type PX = `${number}px`
 type RootMargin = `${PX} ${PX} ${PX} ${PX}` | `${PX} ${PX} ${PX}` | `${PX} ${PX}` | `${PX}`
-type ImageProps = Omit<ImgHTMLAttributes<HTMLImageElement>, 'height' | 'width'>
 
 export type SourceProps = Omit<SourceHTMLAttributes<HTMLSourceElement>, 'src'> & { srcSet: string }
 
-export interface AsyncImageProps extends ImageProps {
+export interface AsyncImageProps extends ImgHTMLAttributes<HTMLImageElement> {
 	src: string
 	sources?: SourceProps[]
 	rootMargin?: RootMargin
