f96c4a5f24eada32b8693a10d43c98f875694ebb
denchiklut <ollylut@gmail.com>
2026-04-19 11:08:22 -0700

    chore: workspace setup

--- docs/.storybook/main.ts
+++ docs/.storybook/main.ts
@@ -1,14 +1,10 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 
-const config: StorybookConfig = {
+export default {
 	stories: ['../src/**/*.stories.@(ts|tsx)'],
 	addons: ['@storybook/addon-links'],
 	framework: {
 		name: '@storybook/react-vite',
 		options: {}
 	},
-	docs: {
-		autodocs: 'tag'
-	}
-}
-export default config
+} satisfies StorybookConfig
